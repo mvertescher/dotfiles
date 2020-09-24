fd2615981a382df525cf952e27839516c34da135
Matt Vertescher <mvertescher@gmail.com>
2020-09-23 22:30:00 -0400

    Introduce a bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+# Install `dot`
 curl -L https://github.com/ubnt-intrepid/dot/releases/download/v0.1.4/dot-v0.1.4-x86_64-unknown-linux-gnu.tar.gz | sudo tar xz -C /usr/local/bin
+
+# Install `tpm`
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
